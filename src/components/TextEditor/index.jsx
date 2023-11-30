7910cbb21d7f4e58f4a27ddf931e9ed9f02f85a8
NoahPhill <noahphillips@unomaha.edu>
2023-11-29 18:47:17 -0600

    Replacing JS prompt with MUI Dialog/adding the ability to download either a .txt or .doc file.

--- src/components/TextEditor/index.jsx
+++ src/components/TextEditor/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useRef, useState } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
-import {Paper} from '@mui/material'
+import {Paper, Select, Button, TextField, MenuItem} from '@mui/material'
 import KeyboardVoiceOutlinedIcon from '@mui/icons-material/KeyboardVoiceOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import StopCircleIcon from '@mui/icons-material/StopCircle';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 const TextEditor = () => {
@@ -24,18 +29,29 @@ const TextEditor = () => {
       }
     }
   ];
-  
+
+   const microphoneRef = useRef(null);
    const { transcript, resetTranscript } = useSpeechRecognition({ commands });
    const [isListening, setIsListening] = useState(false);
    const [concurrentText, setConcurrentText] = useState('');
-   const microphoneRef = useRef(null);
+   const [open, setIsOpen] = useState(false);
+   const [fileName, setFileName] = useState('');
+   const [fileType, setFileType] = useState('');
+
+   const openDialog = () => {
+      setIsOpen(true);
+   };
+
+   const closeDialog = () => {
+      setIsOpen(false);
+   };
    
 
    const editorStyle = {
-      width: '100%',
-      height: '100vw',
-      fontSize: '50px'
-      
+      width: '99.7%',
+      height: '86.7vh',
+      fontSize: '50px',
+      overflow: 'auto'
    };
 
     if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
@@ -68,23 +84,19 @@ const TextEditor = () => {
 
     const downloadFile = async () => {
 
-      let inputtedName = prompt("Input a file-name!");
-
-      if (inputtedName != null && inputtedName.trim().length > 1){
+      if (fileName != null && fileName.trim().length > 1){
         const link = document.createElement("a");
         const content = concurrentText.length === 0 ? transcript : concurrentText;
-        const generatedFile = new Blob([content], { type: 'text/plain' });
+        const generatedFile = new Blob([content], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'});
         link.href = URL.createObjectURL(generatedFile);
-        link.download = inputtedName;
+        link.download = `${fileName}${fileType}`;
         link.click();
         URL.revokeObjectURL(link.href);
-    } else if (inputtedName === null) {
-       return;
+        closeDialog();
     } else {
-      alert('You cannot have an empty file name!');
+       return;
     }
  };
-    
     
     return (
       <Paper className="microphone-wrapper" variant="elevation" elevation={0}>
@@ -102,7 +114,7 @@ const TextEditor = () => {
             </button>
           }
           {!isListening && transcript && 
-            <button title="Download" className="icon-button" onClick={downloadFile}>
+            <button title="Download" className="icon-button" onClick={openDialog}>
               <FileDownloadOutlinedIcon/>
             </button>
           }
@@ -112,9 +124,50 @@ const TextEditor = () => {
             style={editorStyle} 
             className="microphone-result-text" 
             value={concurrentText.length === 0 ? transcript : concurrentText} 
-            onChange={(e) => setConcurrentText(e.target.value)}>
+            onChange={(e) => setConcurrentText(e.target.value)}
+            >
           </textarea>
         </div>
+        <Dialog open={open} onClose={closeDialog}> 
+          <DialogTitle textAlign={"center"}><b>Create File</b></DialogTitle>
+          <DialogContent>
+            <DialogContentText>Enter a file-name below!</DialogContentText>
+          </DialogContent>
+            <DialogActions 
+            autoFocus={true} 
+            sx={{justifyContent: "center"}}
+            > 
+              <TextField 
+              required={true}
+              onKeyDown={e => e.code === "Space" ? e.preventDefault() : e }
+              onChange={e => setFileName(e.target.value)}
+              value={fileName}
+              />
+              <Select>
+                <MenuItem value=".docx" onClick={() => setFileType('.doc')}>.doc</MenuItem>
+                <MenuItem value=".txt" onClick={() => setFileType('.txt')}>.txt</MenuItem>
+              </Select>
+            </DialogActions> 
+            <DialogActions 
+            autoFocus={true} 
+            sx={ {justifyContent: "center"} }
+            >
+              <Button 
+              onClick={closeDialog} 
+              variant="contained" 
+              color="error"
+              >
+                  Close 
+              </Button> 
+              <Button 
+              onClick={downloadFile} 
+              disabled={fileName.length <= 0}
+              variant="contained" 
+              color="success"> 
+                  Create File
+              </Button> 
+            </DialogActions> 
+        </Dialog> 
       </Paper>
     );
 };
